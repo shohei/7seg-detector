061b19eb4c3859c7e44661a96503761fc34bf731
Shohei Aoki <shoaok@gmail.com>
2017-03-08 18:05:41 +0900

    update clicker and trimmer

--- clicker.py
+++ clicker.py
@@ -1,12 +1,40 @@
 import cv2
 
+counter = 0
+x0 = 0
+y0 = 0
+
+class MyCounter:
+    def __init__(self):
+        self.counter = 0
+        self.x0= 0
+        self.y0= 0
+        self.x1= 0
+        self.y1= 0
+
 def mouse_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONUP:
         print "(x,y)= ",x,y
+        if mc.counter==0:
+            mc.x0 = x
+            mc.y0 = y
+            mc.counter = mc.counter+1
+        elif mc.counter==1:
+            mc.x1 = x
+            mc.y1 = y
+            width = mc.x1-mc.x0
+            height = mc.y1-mc.y0
+            print "x = ",mc.x0
+            print "y = ",mc.y0
+            print "width = ",width
+            print "height = ",height
+            mc.counter = 0
 
-img = cv2.imread("target.jpg", 1)
+img = cv2.imread("image.jpg", 1)
 cv2.namedWindow("img")
 cv2.setMouseCallback("img", mouse_event)
+
+mc = MyCounter()
 
 while (True):
     cv2.imshow("img", img)
--- trimmer.py
+++ trimmer.py
@@ -1,10 +1,18 @@
 import cv2
 
-x = 10  
-y = 233 
-width = 180 
-height = 70 
-src = cv2.imread('target.jpg', 1)
+# x = 10  
+# y = 233 
+# width = 180 
+# height = 70 
+# x = 229
+# y = 253
+# width = 138 
+# height =  81
+x =  227
+y =  250
+width =  137
+height =  70
+src = cv2.imread('image.jpg', 1)
 dst = src[y:y+height, x:x+width]
 cv2.imwrite('dst.jpg', dst)
 
