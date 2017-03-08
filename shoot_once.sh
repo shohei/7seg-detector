a49940da9ce9ff80e0fa646a7969dc01eb29cdfc
Shohei Aoki <shoaok@gmail.com>
2017-03-08 09:09:33 +0000

    update

---
+++ shoot_once.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+fswebcam --no-timestamp --no-banner -r 1280x1024 image.jpg
--- trimmer.py
+++ trimmer.py
@@ -4,10 +4,10 @@ import cv2
 #y = 233 
 #width = 180 
 #height = 70 
-x = 172
-y = 245
-width = 138
-height =  81
+x =  227
+y =  250
+width =  137
+height =  70
 src = cv2.imread('image.jpg', 1)
 dst = src[y:y+height, x:x+width]
 cv2.imwrite('dst.jpg', dst)
