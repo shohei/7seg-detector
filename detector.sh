74cc872572239e4da0ee03ad0ef79d32a0057944
Shohei Aoki <shoaok@gmail.com>
2017-03-09 04:56:50 +0000

    update

--- detector.sh
+++ detector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./ssocr -d -1 bwimage.jpg | cat | perl -ne 'print substr($_,0,length($_)-2);print ".";print substr($_,-2);' > z_height.txt
+./ssocr -d -1 bwimage.jpg | cat | perl -ne 'if ($_=~/./) {print $_} else {print substr($_,0,length($_)-2);print ".";print substr($_,-2);}'  > z_height.txt
 
