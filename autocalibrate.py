34bd9ea45455a7671bd84bcff369b5feec3490f0
Shohei Aoki <shoaok@gmail.com>
2017-03-09 12:49:52 +0900

    update z height

--- autocalibrate.py
+++ autocalibrate.py
@@ -57,7 +57,7 @@ def detectNumber():
     #         python trimmer.py; python binarize.py; \
     #         ./ssocr -d -1 bwimage.jpg z_height"
     command = "python trimmer.py; python binarize.py; \
-             ./ssocr -d -1 bwimage.jpg > z_height.txt"
+             ./ssocr -d -1 bwimage.jpg | cat | perl -ne 'print substr($_,0,length($_)-2);print \".\";print substr($_,-2);' > z_height.txt"
     os.system(command)
     _, z_height = commands.getstatusoutput("cat z_height.txt")
     z_height = float(z_height)
