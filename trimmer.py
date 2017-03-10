b2ba03f310543993ad2170155536b5f77f96f7ae
Shohei Aoki <shoaok@gmail.com>
2017-03-10 06:49:04 +0000

    add result

--- trimmer.py
+++ trimmer.py
@@ -4,10 +4,16 @@ import cv2
 #y = 233 
 #width = 180 
 #height = 70 
-x =  242
-y =  258
-width =  133
-height = 75 
+#x =  241
+#y =  256
+#width =  133
+#height =  78
+
+x =  205
+y =  259
+width =  176
+height =  73
+
 src = cv2.imread('image.jpg', 1)
 dst = src[y:y+height, x:x+width]
 cv2.imwrite('dst.jpg', dst)
