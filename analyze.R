a73c67a08d68b02a146122f743b196ad30f42890
Shohei Aoki <shoaok@gmail.com>
2017-03-10 16:11:00 +0900

    modify analyzer

--- analyze.R
+++ analyze.R
@@ -1,10 +1,9 @@
-data.table <- read.table("result.txt",sep=",")
-names(data.table) <- c("rod1","rod2","rod3","X","Y","z_height")
-data.table
+d <- read.table("result.txt",sep=",")
+names(d) <- c("rod1","rod2","rod3","X","Y","z_height")
 rms <- function(x) sqrt(sum(x^2)/length(x))
 res<-aggregate(z_height~rod1+rod2+rod3,data.table,rms) 
 res<-res[order(res$z_height),]
-write.csv(res,file="rms.csv")
+goods=d[d$rod1==-5&d$rod2==0&d$rod3==-3,]
 
-goods=d[d$rod1==-3&d$rod2==0&d$rod3==3,]
+write.csv(res,file="rms.csv")
 write.csv(goods,file="good.csv)
